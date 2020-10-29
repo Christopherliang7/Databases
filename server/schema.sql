a6fa4e167a3182303f7919c5e701ee1b7d77a624
Christopher <Christopherliang7@gmail.com>
2020-10-29 14:08:46 -0400

    ompleted db schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,13 +1,23 @@
+DROP DATABASE chat;
 CREATE DATABASE chat;
 
 USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INTEGER NOT NULL,
+  user_id INTEGER AUTO_INCREMENT NOT NULL,
+  text_message VARCHAR(250) NOT NULL,
+  room_name VARCHAR(100) NOT NULL,
+  PRIMARY KEY (user_id)
 );
 
 /* Create other tables and define schemas for them here! */
 
+CREATE TABLE users (
+  user_id INTEGER NOT NULL,
+  user_name VARCHAR(50) NOT NULL
+);
 
 
 
